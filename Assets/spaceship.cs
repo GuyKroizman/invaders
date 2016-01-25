f9497a628ae3fd9d7adb803624978309b6d1266c
GuyKroizman <kroizguy@gmail.com>
2016-01-25 22:47:26 +0000

    add spaceship sprite and script to move horizontally

---
+++ Assets/spaceship.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spaceship : MonoBehaviour {
+
+    public float m_speed = 4.0f;
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += move * m_speed * Time.deltaTime;
+    }
+}
